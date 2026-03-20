70dfa211dd2b1301033d6a945472bea4ce1604d7
Vivien Ogoun <vivien.ogoun@epitech.eu>
2026-03-20 06:52:42 +0100

    backend: configure cors

--- Backend/it-num-assessment/src/main/java/com/vogoun/it_num_assessment/ItNumAssessmentApplication.java
+++ Backend/it-num-assessment/src/main/java/com/vogoun/it_num_assessment/ItNumAssessmentApplication.java
@@ -1,13 +1,33 @@
 package com.vogoun.it_num_assessment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItNumAssessmentApplication {
 
+	@Value("${app.cors.allowed-origins}")
+	private String allowedOrigins;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ItNumAssessmentApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(allowedOrigins)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
--- Backend/it-num-assessment/src/main/java/com/vogoun/it_num_assessment/security/WebSecurityConfig.java
+++ Backend/it-num-assessment/src/main/java/com/vogoun/it_num_assessment/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,8 +42,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
                 .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(
                         sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
